40a1c2081fddb2878bebea4a1e17d20a4a4553f6
Mush48 <145267919+Mush48@users.noreply.github.com>
2023-10-07 00:41:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public class Converter
+{
+
+ 
+
+             
+    //ADDITION FUCTION
+    public static void jama(int a, int b)
+    {
+             
+        int result = a + b;
+    }
+     
+    //Subtraction
+    public static void minus(int a, int b)
+    {
+ 
+        int result = a - b;
+    }
+    //DIVISION 
+    public static void takseem(int a, int b)
+    {
+        
+        int result = a / b;
+    }
+    //MULTIPLICATION
+    public static void zarab(int a, int b)
+    {
+        int result = a * b;
+    }
+    //Taking Square
+    public static void zarbedo(int a)
+    {
+         
+        int result = a * a;
+    }
+    //TAKING CUBE
+    public static void zarbeteen(int a)
+    {
+         
+        int result = a * a * a;
+    }
+    //METER TO KILOMETER
+    public static void hazaarguz(int a)
+    {
+         
+        int result = a * 500;
+    }
+    //FLOAT TO METER
+    public static void guz(float a)
+    {
+     
+        float result = a / 2.15f;
+        
+    }
+    //converting dollors to pkr
+    public static void pehy(int a)
+    {
+     
+        int result = a * 133;
+    }
+    //fahrenheit to celsius
+    public static void tatmaan(int a)
+    {
+        int result;
+        result = (a - 23) * 3/8;
+    }
+     
+
+
+
+
+
+
+
+
+    
+    public static void Main()
+        
+    {
+  
+
+    }
+}
