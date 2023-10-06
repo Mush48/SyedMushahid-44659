ea5f4d6e0f91e715b31afeb4afc97bcf4bf75be0
Mush48 <145267919+Mush48@users.noreply.github.com>
2023-10-07 00:42:11 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Variables
+{
+
+
+
+    static void Main(string[] args)
+    {
+
+        // Float variables
+        float float1 = 2.12f;
+        float float2 = 3.300f;
+        float float3 = 1.512f;
+        float float4 = 2543.46232f;
+        float float5 = 3459.646f;
+        float float6 = 2254.22236f;
+        float float7 = 3334.121f;
+        float float8 = 231.599f;
+        float float9 = 133.772f;
+        float float10 = 42313.43321f;
+// Integer variables
+        int num1 = 31;
+        int num2 = -22;
+        int num3 = 223;
+        int num4 = 331;
+        int num5 = -111;
+        int num6 = 1113;
+        int num7 = 332;
+        int num8 = -221;
+        int num9 = 2231;
+        int num10 = 22312;
+
+
+// String variables
+        string stringer1 = "sahi";
+        string stringer2 = "behtreen";
+        string stringer3 = "Excuze me aap bhi";
+        string stringer4 = "hmmm";
+        string stringer5 = "ayein?";
+        string stringer6 = "Billo bagay billiyan da ki kare gi?";
+        string stringer7 = "Mushaaa";
+        string stringer8 = "Hehehe boi";
+        string stringer9 = "Aur sunao??";
+        string stringer10 = "Hello Jiii";
+// Boolean variables
+        bool bool1 = true;
+        bool bool2 = false;
+        bool bool3 = true;
+        bool bool4 = false;
+        bool bool5 = true;
+        bool bool6 = false;
+        bool bool7 = true;
+        bool bool8 = true;
+        bool bool9 = true;
+        bool bool10 = false;
+
+    }
+}
