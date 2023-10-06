ec31d5ea9ca4d23e845f2a2ecaf41956a2512f98
Mush48 <145267919+Mush48@users.noreply.github.com>
2023-10-07 00:40:27 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        public static int Result;
+        public static void Add(int A, int B)
+        {
+            Result = A + B;
+        }
+
+        public static void Subtract(int A, int B)
+        {
+            Result = A - B;
+        }
+
+        public static void Multiply(int A, int B)
+        {
+            Result = A * B;
+        }
+
+        public static void Divide(int A, int B)
+        {
+            Result = A / B;
+        }
+
+        public static void Main(string[] args)
+        {
+            Add(5, 15);
+            Console.WriteLine("Answer is: " + Result);
+
+            Subtract(90, 25);
+            Console.WriteLine("Answer is: " + Result);
+
+            Multiply(6, 3);
+            Console.WriteLine("Answer is: " + Result);
+
+            Divide(30,5);
+            Console.WriteLine("Answer is: " + Result);
+            
+
+        }
+    }
+}
